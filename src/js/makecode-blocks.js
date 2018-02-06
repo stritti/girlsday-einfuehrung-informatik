e03d37947fe1b775a94c1aed38462fa17eb275f2
stritti <stephan@st-strittmatter.name>
2018-02-06 21:49:07 +0100

    * Updated rendering Blocks. See: https://github.com/Microsoft/pxt-microbit/issues/623 * Improved Readme

--- src/js/makecode-blocks.js
+++ src/js/makecode-blocks.js
@@ -1,36 +1,90 @@
+//use strict
+/* message signatures
 
-
-
-function renderSnippets() {
-   var codeElems = $('code')
-   for (var i = 0; i < codeElems.length; i++) {
-       codeElems[i].className = codeElems[i].className.replace('-ignore', '')
-   }
-
-   var downloadScreenshots = /screenshots=1/i.test(window.location.href);
-   var path = window.location.href.split('/').pop().split(/[?#]/)[0];
-   ksRunnerReady(function () {
-       pxt.runner.renderAsync({
-           snippetClass: 'lang-blocks',
-           signatureClass: 'lang-sig',
-           blocksClass: 'lang-block',
-           shuffleClass: 'lang-shuffle',
-           simulatorClass: 'lang-sim',
-           linksClass: 'lang-cards',
-           namespacesClass: 'lang-namespaces',
-           codeCardClass: 'lang-codecard',
-           packageClass: 'lang-package',
-           projectClass: 'lang-project',
-           snippetReplaceParent: true,
-           simulator: true,
-           hex: true,
-           hexName: path,
-           downloadScreenshots: downloadScreenshots
-       }).done();
-   });
+export interface RenderBlocksRequestMessage extends SimulatorMessage {
+    type: "renderblocks",
+    id: string;
+    code: string;
+    options?: {
+        package?: string;
+        snippetMode?: boolean;
+    }
 }
 
-$(document).ready(function () {   
-   renderSnippets();
+export interface RenderBlocksResponseMessage extends SimulatorMessage {
+    source: "makecode",
+    type: "renderblocks",
+    id: string;
+    svg?: string;
+    width?: number;
+    height?: number;
+}*/
 
-});
+function makeCodeRenderPre(pre) {
+    "use strict";
+    var 
+        f = document.getElementById("makecoderenderer"),
+        isSnippet = (pre.getAttribute("data-snippet") === null) ? "true" : pre.getAttribute("data-snippet");
+    console.log("isSnippet=" + isSnippet);
+    f.contentWindow.postMessage({
+        type: "renderblocks",
+        id: pre.id,
+        code: pre.innerText,
+        options: {
+            snippetMode: (isSnippet == "true")
+        }
+    }, "https://makecode.microbit.org/");
+}
+
+function renderSnippets() {
+    "use strict";
+ 	// TODO ADD RENDER LOGIC HERE
+    let pres = document.getElementsByTagName("code");
+    Array.prototype.forEach.call(pres, function(pre) {
+        makeCodeRenderPre(pre);
+    });
+}
+
+function makeCodeInjectRenderer() {
+    "use strict";
+    var f = document.createElement("iframe");
+    f.id = "makecoderenderer";
+    f.style.position = "absolute";
+    f.style.left = 0;
+    f.style.bottom = 0;
+    f.style.width = "1px";
+    f.style.height = "1px";            
+    f.src = "https://makecode.microbit.org/--docs?render=1";
+    document.body.appendChild(f);
+}
+
+// listen for messages
+window.addEventListener("message", function (ev) {
+
+    "use strict";
+    var msg = ev.data;
+    if (msg.source != "makecode") return;
+
+    switch (msg.type) {
+        case "renderready":
+            // start rendering snippets!
+            renderSnippets();
+            break;
+        case "renderblocks":
+            var svg = msg.svg; // this is an string containing SVG
+            var id = msg.id; // this is the id you sent
+            // replace text with svg
+            var img = document.createElement("img");
+            img.src = msg.uri;
+            img.width = msg.width;
+            img.height = msg.height;
+            var code = document.getElementById(id);
+            code.parentElement.insertBefore(img, code);
+            code.parentElement.removeChild(code);
+            break;
+    }
+}, false);
+
+
+// load the renderer
+makeCodeInjectRenderer();
