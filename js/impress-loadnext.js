764f612555bfeab32e0eb9ca896b54470a2d2f82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-26 15:44:45 +0000

    Deploy stritti/girlsday-einfuehrung-informatik to github.com/stritti/girlsday-einfuehrung-informatik.git:gh-pages

--- js/impress-loadnext.js
+++ js/impress-loadnext.js
@@ -4,19 +4,22 @@
  */
 
 /* global document, navigator */
-(function (document) {
-	"use strict";
+;(function (document) {
+  'use strict'
 
-	// load next page if attribute is set
-	document.addEventListener("impress:stepenter", function (event) {
-
-		var nextImpress = event.srcElement.dataset.nextImpress;
-		console.log(event);
-		if ( nextImpress !== undefined ) {
-			console.log("Load next impress file: " + nextImpress);
-			document.addEventListener( "impress:stepleave", function() {
-				window.location = nextImpress;
-		  }, false );
-		}
-	});
-})(document);
+  // load next page if attribute is set
+  document.addEventListener('impress:stepenter', function (event) {
+    var nextImpress = event.srcElement.dataset.nextImpress
+    console.log(event)
+    if (nextImpress !== undefined) {
+      console.log('Load next impress file: ' + nextImpress)
+      document.addEventListener(
+        'impress:stepleave',
+        function () {
+          window.location = nextImpress
+        },
+        false
+      )
+    }
+  })
+})(document)
--- js/jquery.makecode-blocks.js
+++ js/jquery.makecode-blocks.js
@@ -1,42 +1,39 @@
 /*!
  * jQuery MakerCode Blocks
- * 
- * Rendering Block elemenst by given scripts. 
+ *
+ * Rendering Block elemenst by given scripts.
  * version: 1.0.0
  *
  */
-/* eslint-disable */
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
-	} else if (typeof module === 'object' && module.exports) {
-		// Node/CommonJS
-		module.exports = function( root, jQuery ) {
-			if (typeof jQuery === 'undefined') {
-				// require('jQuery') returns a factory that requires window to build a jQuery instance, we normalize how we use modules
-				// that require this pattern but the window provided is a noop if it's defined (how jquery works)
-				if (typeof window !== 'undefined') {
-					jQuery = require('jquery');
-				}
-				else {
-					jQuery = require('jquery')(root);
-				}
-			}
-			factory(jQuery);
-			return jQuery;
-		};
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
+/* global jQuery, define */
+;(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = function (root, jQuery) {
+      if (typeof jQuery === 'undefined') {
+        // require('jQuery') returns a factory that requires window to build a jQuery instance, we normalize how we use modules
+        // that require this pattern but the window provided is a noop if it's defined (how jquery works)
+        if (typeof window !== 'undefined') {
+          jQuery = require('jquery')
+        } else {
+          jQuery = require('jquery')(root)
+        }
+      }
+      factory(jQuery)
+      return jQuery
+    }
+  } else {
+    // Browser globals
+    factory(jQuery)
+  }
+})(function ($) {
+  /* eslint-enable */
+  'use strict'
 
-}(function ($) {
-/* eslint-enable */
-	'use strict';
-
- 
-    /* message signatures
+  /* message signatures
 
     export interface RenderBlocksRequestMessage extends SimulatorMessage {
         type: "renderblocks",
@@ -55,72 +52,76 @@
         svg?: string;
         width?: number;
         height?: number;
-    }*/
+    } */
 
+  $.fn.blocks = function () {
+    // listen for messages
+    window.addEventListener(
+      'message',
+      function (ev) {
+        var msg = ev.data
+        if (msg.source !== 'makecode') return
 
-    $.fn.blocks = function() {
-        // listen for messages
-        window.addEventListener("message", function (ev) {
+        switch (msg.type) {
+          case 'renderready':
+            // start rendering snippets!
+            $.fn.renderSnippets()
+            break
+          case 'renderblocks':
+            // var svg = msg.svg // this is an string containing SVG
+            var id = msg.id // this is the id you sent
+            // replace text with svg
+            var img = document.createElement('img')
+            img.src = msg.uri
+            img.width = msg.width
+            img.height = msg.height
+            var code = document.getElementById(id)
+            code.parentElement.insertBefore(img, code)
+            code.parentElement.removeChild(code)
+            break
+        }
+      },
+      false
+    )
 
-            var msg = ev.data;
-            if (msg.source != "makecode") return;
+    // load the renderer
+    $.fn.makeCodeInjectRenderer()
+  }
 
-            switch (msg.type) {
-                case "renderready":
-                    // start rendering snippets!
-                    $.fn.renderSnippets();
-                    break;
-                case "renderblocks":
-                    var svg = msg.svg; // this is an string containing SVG
-                    var id = msg.id; // this is the id you sent
-                    // replace text with svg
-                    var img = document.createElement("img");
-                    img.src = msg.uri;
-                    img.width = msg.width;
-                    img.height = msg.height;
-                    var code = document.getElementById(id);
-                    code.parentElement.insertBefore(img, code);
-                    code.parentElement.removeChild(code);
-                    break;
-            }
-        }, false);  
-                
-        // load the renderer
-        $.fn.makeCodeInjectRenderer();
-    };
+  $.fn.makeCodeRenderPre = function ($block) {
+    var f = document.getElementById('makecoderenderer')
 
-    $.fn.makeCodeRenderPre = function($block) {
-        var 
-            f = document.getElementById("makecoderenderer"),
-            isSnippet = ($block.attr("data-snippet") === null) ? "true" : $block.attr("data-snippet");
+    var isSnippet = $block.attr('data-snippet') === null ? 'true' : $block.attr('data-snippet')
 
-        f.contentWindow.postMessage({
-            type: "renderblocks",
-            id: $block.attr('id'),
-            code: $block.text(),
-            options: {
-                snippetMode: (isSnippet == "true")
-            }
-        }, "https://makecode.microbit.org/");
-    };
+    f.contentWindow.postMessage(
+      {
+        type: 'renderblocks',
+        id: $block.attr('id'),
+        code: $block.text(),
+        options: {
+          snippetMode: isSnippet === 'true'
+        }
+      },
+      'https://makecode.microbit.org/'
+    )
+  }
 
-    $.fn.renderSnippets = function() {
-        var $blocks = $(".js-blocks");
-        Array.prototype.forEach.call($blocks, function(block) {
-            $.fn.makeCodeRenderPre($(block));
-        });
-    };
+  $.fn.renderSnippets = function () {
+    var $blocks = $('.js-blocks')
+    Array.prototype.forEach.call($blocks, function (block) {
+      $.fn.makeCodeRenderPre($(block))
+    })
+  }
 
-    $.fn.makeCodeInjectRenderer = function() {
-        var f = document.createElement("iframe");
-        f.id = "makecoderenderer";
-        f.style.position = "absolute";
-        f.style.left = 0;
-        f.style.bottom = 0;
-        f.style.width = "1px";
-        f.style.height = "1px";            
-        f.src = "https://makecode.microbit.org/--docs?render=1";
-        document.body.appendChild(f);
-    };
-
-}));
+  $.fn.makeCodeInjectRenderer = function () {
+    var f = document.createElement('iframe')
+    f.id = 'makecoderenderer'
+    f.style.position = 'absolute'
+    f.style.left = 0
+    f.style.bottom = 0
+    f.style.width = '1px'
+    f.style.height = '1px'
+    f.src = 'https://makecode.microbit.org/--docs?render=1'
+    document.body.appendChild(f)
+  }
+})
